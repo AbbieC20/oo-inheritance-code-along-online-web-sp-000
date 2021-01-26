4327811ffbe43ea35d1bdce206920c118819a361
remote-torrent-6747 <abbyc@sky.com>
2021-01-26 20:25:25 +0000

    Automatically backed up by Learn

--- lib/vehicle.rb
+++ lib/vehicle.rb
@@ -5,13 +5,16 @@ class Vehicle
     @wheel_size = wheel_size
     @wheel_number = wheel_number
   end
+  #initialize with a wheel size and number
 
   def go
     "vrrrrrrrooom!"
   end
+  #common vehicle behaviour
 
   def fill_up_tank
     "filling up!"
   end
+  #common vehicle behaviour
   
 end
